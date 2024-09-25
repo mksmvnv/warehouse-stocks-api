0529a0f084f320bc8d1f600d3cf4a44fcbff34d6
Maksim <mksmvnv@icloud.com>
2024-09-25 16:54:57 +0300

    Add models

---
+++ db/models.py
@@ -0,0 +1,46 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Enum
+from sqlalchemy.orm import relationship
+
+from datetime import datetime, timezone
+
+from db.connect import Base
+
+
+class OrderStatusEnum(enum.Enum):
+    in_progress = "в процессе"
+    shipped = "отправлен"
+    delivered = "доставлен"
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, default=0)
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    status = Column(Enum(OrderStatusEnum), default=OrderStatusEnum.in_progress)
+    
+    order_items = relationship("OrderItem", back_populates="order")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer, nullable=False)
+
+    order = relationship("Order", back_populates="order_items")
+    product = relationship("Product")

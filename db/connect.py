bb636874e1ce2fb0ff35ed08b3abe6584e50243b
Maksim <mksmvnv@icloud.com>
2024-09-25 19:57:55 +0300

    Add init db

--- db/connect.py
+++ db/connect.py
@@ -19,7 +19,13 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)

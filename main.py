66544fd0e47b193c4e503386d0cbb75258bbc1a7
Maksim <mksmvnv@icloud.com>
2024-09-26 11:44:44 +0300

    Add routers and import init db

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 
+from db.connect import init_db
+
+from routers import order_router, product_router
+
 
 app = FastAPI()
 
+init_db()
+
+app.include_router(product_router.router)
+app.include_router(order_router.router)

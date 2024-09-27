a73e8b0cfafd01cd67771bac902d65c0e1c9bf25
Maksim <mksmvnv@icloud.com>
2024-09-27 10:34:17 +0300

    Add api secret prefix

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from db.connect import init_db
 
 from routers import order_router, product_router
 
+from config import settings
+
 
 app = FastAPI()
 
 init_db()
 
-app.include_router(product_router.router)
-app.include_router(order_router.router)
+app.include_router(product_router.router, prefix=settings.api_prefix)
+app.include_router(order_router.router, prefix=settings.api_prefix)
